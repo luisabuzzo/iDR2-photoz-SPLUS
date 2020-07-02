80423c4747aece8328dd8d53ba3cbe59660d6520
Maria Luísa Buzzo <luisa.buzzo@gmail.com>
2020-07-02 15:13:39 -0300

    Number counts around bright stars

--- plot_numbercounts.py
+++ plot_numbercounts.py
@@ -38,7 +38,7 @@ def sel_cat(rad,cat_stars,cat_splus,filt,photoflag):
 	mag_star = []
 	mag_obj = []
 	for i in idx_1:
-		mag_star.append(starcat['B2mag'][i])
+		mag_star.append(starcat['Pmag'][i])
 	for j in idx_2:
 		mag_obj.append(spluscat['r_auto'][j])
 	table = Table([idx_1,mag_star, idx_2,mag_obj, d2d, d3d],names=['index_starcoord','mag_star','index_spluscoord','mag_obj','distance_2d','distance_3d'])
@@ -62,16 +62,16 @@ def plot_radial_bins2(r,cat_stars,cat_splus,filt,photoflag,magstar_min,magstar_m
 		d = {'col1': file2['index_spluscoord'], 'col2': globals()["Counts_magobj" + str(j)],'col3': file2['index_starcoord']}
 		globals()["data" + str(j)] = pd.DataFrame(data=d)
 		
-	return globals()["data" + str(1)],globals()["data" + str(2)],globals()["data" + str(3)],globals()["data" + str(4)],globals()["data" + str(5)]
+	return globals()["data" + str(0)],globals()["data" + str(1)],globals()["data" + str(2)],globals()["data" + str(3)],globals()["data" + str(4)],globals()["data" + str(5)]
 
 def main():
 
-	catstars = os.path.join(home,'data/iDR2/STRIPE82_USNO_stars_broad.fits')
+	catstars = os.path.join(home,'data/iDR2/match_STRIPE82_GSC.fits')
 	catfile = os.path.join(home,'data/iDR2/STRIPE82_magtest_broad.fits')
 	filt = 'r'
 	photoflag = 0
 
-	magstar_min = np.arange(14.0,15.0,1)
+	magstar_min = np.arange(10.0,15.0,1)
 
 	radius = np.arange(4,42,2)
 	label = ['mag_obj < 17','mag_obj < 18','mag_obj < 19','mag_obj < 20','mag_obj < 21','mag_obj < 22']
@@ -80,12 +80,12 @@ def main():
 	for i in magstar_min:
 		print(i)
 		plt.figure()
-		for j in range(5):
+		for j in range(6):
 			number_dens = []
 			for r in radius:
 				# plot_radial_bins2(r,catstars,catfile,filt,photoflag,i,i+1)
-				df0,df1,df2,df3,df4 = plot_radial_bins2(r,catstars,catfile,filt,photoflag,i,i+1)
-				df0_in,df1_in,df2_in,df3_in,df4_in = plot_radial_bins2(r-2,catstars,catfile,filt,photoflag,i,i+1)
+				df0,df1,df2,df3,df4,df5 = plot_radial_bins2(r,catstars,catfile,filt,photoflag,i,i+1)
+				df0_in,df1_in,df2_in,df3_in,df4_in,df5_in = plot_radial_bins2(r-2,catstars,catfile,filt,photoflag,i,i+1)
 				print(len(df4))
 		
 				area = np.pi*(r)**2 - np.pi*(r-2)**2
